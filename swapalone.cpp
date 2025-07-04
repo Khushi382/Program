fd6a4264ffb250cdd78e44e79181010cc01caa80
Khushi382 <ky20057777@gmail.com>
2025-07-04 12:29:33 +0530

    swap

---
+++ swapalone.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int swap(int a,int b){
+    a=a+b;
+    b=a-b;
+    a=a-b;
+    cout<<"value of a "<<a<<endl;
+    cout<<"value of b "<<b<<endl;
+}
+
+int main() {
+    int a;
+    cout<<"Enter a"<<endl;
+    cin>>a;
+    int b;
+    cout<<"Enter b"<<endl;
+    cin>>b;
+    int result= swap(a,b);
+
+
+    return 0;
+}
